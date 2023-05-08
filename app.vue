cfeae1c952579cbb00849378fdb500e3d7f1d1a0
김주찬 <rlawncks125@naver.com>
2023-05-08 15:57:02 +0900

    [환경 추가]tailwind css

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "타이틀 테스트",
+  ogTitle: "OG 타이틀 이에요",
+});
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="text-red-500">
+    hi
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<style lang="scss" scoped></style>
