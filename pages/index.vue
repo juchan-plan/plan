ed58eb1baea9e63325012339b44e23f90fc902c5
김주찬 <rlawncks125@naver.com>
2023-05-15 15:00:11 +0900

    .

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,6 @@
   <div>
     <Count />
     <br />
-    <Icon class="text-[3rem]" name="logos:vue" />
-    <Icon class="text-[3rem] inline-block" name="logos:adobe-photoshop" />
   </div>
 </template>
 
