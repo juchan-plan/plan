43148d58b73299a5ae4e6902e474ae7b1c5595d6
김주찬 <rlawncks125@naver.com>
2023-06-25 11:59:12 +0900

    pr 되라

--- components/__tests__/NewCompo.spec.ts
+++ components/__tests__/NewCompo.spec.ts
@@ -11,5 +11,7 @@ describe("<NewCompo />", () => {
 
   it("Rneder", () => {
     expect(wrapper.text()).toContain("newCompo");
+    expect(wrapper.text()).toContain("newCompo");
+    expect(wrapper.text()).toContain("newCompo");
   });
 });
