1666b34d94e8f8cf1e98f9659593735bae616274
김주찬 <rlawncks125@naver.com>
2023-06-25 09:24:59 +0900

    index 정리

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,6 @@
     <br />
     <Icon class="text-[3rem]" name="teenyicons:arrow-outline" />
     <Icon class="text-[3rem] inline-block" name="logos:adobe-photoshop" />
-    <ClientOnly>
-      <Trigger />
-    </ClientOnly>
   </div>
 </template>
 
